70097f246d9fa2fc30314987450cda8697b0dd9d
aakankshak7777 <167679345+aakankshak7777@users.noreply.github.com>
2024-04-22 15:00:48 +0530

    add seller service implementation class

---
+++ src/main/java/com/kmbl/InventoryManagementService/Service/SellerServiceImpl.java
@@ -0,0 +1,33 @@
+package com.kmbl.InventoryManagementService.Service;
+
+import com.kmbl.InventoryManagementService.models.Seller;
+
+import java.util.List;
+public class SellerServiceImpl implements  SellerServiceInterface{
+
+
+    @Override
+    public String createSeller(Seller seller) {
+        return null;
+    }
+
+    @Override
+    public String updateSeller(Seller seller) {
+        return null;
+    }
+
+    @Override
+    public String deleteSeller(String sellerID) {
+        return null;
+    }
+
+    @Override
+    public Seller getSeller(String sellerID) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> getAllsellers() {
+        return List.of();
+    }
+}
