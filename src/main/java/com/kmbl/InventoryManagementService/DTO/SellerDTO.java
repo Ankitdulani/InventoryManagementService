44deb907ba6ff33ecadfb8cbf75eb985bc43f370
aakankshak7777 <167679345+aakankshak7777@users.noreply.github.com>
2024-04-22 16:04:45 +0530

    added seller dto class

---
+++ src/main/java/com/kmbl/InventoryManagementService/DTO/SellerDTO.java
@@ -0,0 +1,11 @@
+package com.kmbl.InventoryManagementService.DTO;
+
+import lombok.Data;
+
+@Data
+public class SellerDTO {
+    private String id;
+    private String name;
+    private String address;
+    private double  rating;
+}
